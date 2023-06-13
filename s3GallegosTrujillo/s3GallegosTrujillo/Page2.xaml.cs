4afbfd2b24d34a420f5ac8f41dc5cbd3dae05d1b
master <master@SMLAB310PC15_23>
2023-06-12 19:21:19 -0500

    Agregar archivos de proyecto.

---
+++ s3GallegosTrujillo/s3GallegosTrujillo/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace s3GallegosTrujillo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2( string datoUno, int datoDos)
+        {
+            InitializeComponent();
+            lblDatoUno.Text = "Dato Uno"+datoUno;
+            lblDatoDos.Text = "Datos Dos"+datoDos.ToString();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
